0b17050cb1197ca6dfddbad013e7dd8066e67158
rajan_2806 <rajan.goswami85@gmail.com>
2013-12-08 20:47:37 +0530

    Request screen is developed. Menu in top actionbar incorporated.

---
+++ src/com/codeperf/restclient/ui/AddHeaderDialog.java
@@ -0,0 +1,105 @@
+package com.codeperf.restclient.ui;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+
+import com.codeperf.restclient.R;
+
+public class AddHeaderDialog extends DialogFragment {
+
+	/*
+	 * The activity that creates an instance of this dialog fragment must
+	 * implement this interface in order to receive event callbacks. Each method
+	 * passes the DialogFragment in case the host needs to query it.
+	 */
+	public interface AddHeaderDialogListener {
+		public void onDialogPositiveClick(DialogFragment dialog,
+				String headerName, String headerValue);
+
+		public void onDialogNegativeClick(DialogFragment dialog, Bundle bundle);
+	}
+
+	// Use this instance of the interface to deliver action events
+	AddHeaderDialogListener mListener = null;
+
+	@Override
+	public Dialog onCreateDialog(Bundle savedInstanceState) {
+		// Get the layout inflater
+		LayoutInflater inflater = getActivity().getLayoutInflater();
+
+		final View v = inflater.inflate(R.layout.layout_add_header, null);
+
+		Bundle args = getArguments();
+		if (args != null) {
+			if (args.getBoolean("HEADER_MODIFY_FLAG")) {
+				EditText etHeaderName = (EditText) v
+						.findViewById(R.id.et_header_name);
+				EditText etHeaderValue = (EditText) v
+						.findViewById(R.id.et_header_value);
+				Log.d("RESTClient", " etHeaderName - " + etHeaderName);
+				etHeaderName.setText(args.getString("HEADER_NAME"));
+				etHeaderValue.setText(args.getString("HEADER_VALUE"));
+			}
+		}
+
+		return new AlertDialog.Builder(getActivity())
+				.setView(v)
+				.setTitle(R.string.title_header_dialog)
+				.setNegativeButton(R.string.bt_text_cancel,
+						new OnClickListener() {
+
+							@Override
+							public void onClick(DialogInterface dialog,
+									int which) {
+								if (mListener != null)
+									mListener.onDialogNegativeClick(
+											AddHeaderDialog.this, null);
+							}
+						})
+				.setPositiveButton(R.string.bt_text_okay,
+						new OnClickListener() {
+
+							@Override
+							public void onClick(DialogInterface dialog,
+									int which) {
+								if (mListener != null) {
+									EditText etHeaderName = (EditText) getDialog()
+											.findViewById(R.id.et_header_name);
+									EditText etHeaderValue = (EditText) getDialog()
+											.findViewById(R.id.et_header_value);
+									mListener.onDialogPositiveClick(
+											AddHeaderDialog.this, etHeaderName
+													.getText().toString(),
+											etHeaderValue.getText().toString());
+								}
+								dialog.dismiss();
+							}
+						}).create();
+	}
+
+	@Override
+	public void onAttach(Activity activity) {
+		super.onAttach(activity);
+
+		// Verify that the host activity implements the callback interface
+		try {
+			// Instantiate the AddHeaderDialogListener so we can send events to
+			// the host
+			mListener = (AddHeaderDialogListener) activity;
+
+		} catch (ClassCastException e) {
+			// The activity doesn't implement the interface, throw exception
+			throw new ClassCastException(activity.toString()
+					+ " must implement AddHeaderDialogListener");
+		}
+	}
+}
--- src/com/codeperf/restclient/ui/MainActivity.java
+++ src/com/codeperf/restclient/ui/MainActivity.java
@@ -1,21 +1,26 @@
 package com.codeperf.restclient.ui;
 
 import android.app.ActionBar;
+import android.app.DialogFragment;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.codeperf.restclient.R;
 
 public class MainActivity extends FragmentActivity implements
-		ActionBar.TabListener {
+		ActionBar.TabListener, AddHeaderDialog.AddHeaderDialogListener {
 
 	/**
 	 * The serialization (saved instance state) Bundle key representing the
@@ -23,21 +28,28 @@ public class MainActivity extends FragmentActivity implements
 	 */
 	private static final String STATE_SELECTED_NAVIGATION_ITEM = "selected_navigation_item";
 
+	// Constant for identifying the dialog
+	private static final int DIALOG_ADD_HEADER = 10;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
 		setContentView(R.layout.activity_main);
 
 		// Set up the action bar to show tabs.
 		final ActionBar actionBar = getActionBar();
 		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 
+		/*
+		 * actionBar.setDisplayShowTitleEnabled(false);
+		 * actionBar.setDisplayShowHomeEnabled(false);
+		 */
+
 		// For each of the sections in the app, add a tab to the action bar.
 		actionBar.addTab(actionBar.newTab().setText(R.string.title_section1)
 				.setTabListener(this));
 		actionBar.addTab(actionBar.newTab().setText(R.string.title_section2)
-				.setTabListener(this));
-		actionBar.addTab(actionBar.newTab().setText(R.string.title_section3)
 				.setTabListener(this));
 	}
 
@@ -62,6 +74,23 @@ public class MainActivity extends FragmentActivity implements
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.menu_add_header:
+			DialogFragment headerDialog = new AddHeaderDialog();
+			Bundle args = new Bundle();
+			args.putBoolean("HEADER_MODIFY_FLAG", false);
+			headerDialog.setArguments(args);
+			headerDialog.show(getFragmentManager(), "Request Header");
+
+			return true;
+		default:
+			break;
+		}
+		return super.onOptionsItemSelected(item);
 	}
 
 	@Override
@@ -99,7 +128,13 @@ public class MainActivity extends FragmentActivity implements
 		 */
 		public static final String ARG_SECTION_NUMBER = "section_number";
 
+		@Override
+		public void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+		}
+
 		public DummySectionFragment() {
+
 		}
 
 		@Override
@@ -115,4 +150,108 @@ public class MainActivity extends FragmentActivity implements
 		}
 	}
 
+	@Override
+	public void onDialogPositiveClick(DialogFragment dialog, String headerName,
+			String headerValue) {
+
+		if (headerName == null || headerName.isEmpty()) {
+			return;
+		}
+
+		if (headerValue == null)
+			headerValue = "";
+
+		Bundle args = dialog.getArguments();
+		if (args != null) {
+			if (!args.getBoolean("HEADER_MODIFY_FLAG")) {
+
+				// Prepeare a new view and attach to linear_headers_layout
+				final LinearLayout headersLayout = (LinearLayout) findViewById(R.id.linear_headers_layout);
+
+				if (headersLayout.getVisibility() == View.GONE)
+					headersLayout.setVisibility(View.VISIBLE);
+
+				// Get the layout inflater
+				LayoutInflater inflater = getLayoutInflater();
+				LinearLayout headerView = (LinearLayout) inflater.inflate(
+						R.layout.headers_view, null);
+
+				LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+						200, 40);
+				params.leftMargin = 16;
+				params.bottomMargin = 12;
+				params.topMargin = 2;
+
+				final int headerId = i++;
+				headerView.setId(headerId);
+				headerView.setBackgroundResource(R.drawable.header_view_shape);
+				headerView.setOrientation(LinearLayout.HORIZONTAL);
+				headerView.setLayoutParams(params);
+
+				TextView tvHeader = (TextView) headerView
+						.findViewById(R.id.tv_header);
+				tvHeader.setText(headerName + ":" + headerValue);
+				tvHeader.setOnClickListener(new View.OnClickListener() {
+
+					@Override
+					public void onClick(View v) {
+
+						DialogFragment headerDialog = new AddHeaderDialog();
+						Bundle args = new Bundle();
+						args.putBoolean("HEADER_MODIFY_FLAG", true);
+						args.putInt("HEADER_RESOURCE_ID", headerId);
+
+						TextView tv = (TextView) v;
+						String text = tv.getText().toString();
+						String splits[] = text.split(":", 2);
+						Log.d("RESTClient", splits[0]);
+						Log.d("RESTClient", splits[1]);
+						args.putString("HEADER_NAME", splits[0]);
+						args.putString("HEADER_VALUE", splits[1]);
+						headerDialog.setArguments(args);
+						headerDialog.show(getFragmentManager(),
+								"Request Header");
+					}
+				});
+
+				ImageView cancel = (ImageView) headerView
+						.findViewById(R.id.bt_cancel_header);
+				cancel.setOnClickListener(new View.OnClickListener() {
+
+					@Override
+					public void onClick(View v) {
+						LinearLayout headerView = (LinearLayout) headersLayout
+								.findViewById(headerId);
+						((ViewGroup) headersLayout).removeView(headerView);
+
+						Log.d("RESTClient",
+								"ChildCount ==" + headersLayout.getChildCount());
+						if (headersLayout.getChildCount() == 1) {
+							headersLayout.setVisibility(View.GONE);
+						}
+					}
+				});
+				((ViewGroup) headersLayout).addView(headerView);
+			} else {
+				// Modifying existing header
+				LinearLayout headersLayout = (LinearLayout) findViewById(R.id.linear_headers_layout);
+
+				LinearLayout headerView = (LinearLayout) headersLayout
+						.findViewById(args.getInt("HEADER_RESOURCE_ID"));
+				if (headerView != null) {
+					TextView tvHeader = (TextView) headerView
+							.findViewById(R.id.tv_header);
+					tvHeader.setText(headerName + ":" + " " + headerValue);
+				}
+			}
+		}
+	}
+
+	private static int i = 0;
+
+	@Override
+	public void onDialogNegativeClick(DialogFragment dialog, Bundle bundle) {
+		// TODO Auto-generated method stub
+
+	}
 }
